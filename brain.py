614a2473edd2881c7e69c6f95db058b673753226
LightRoses <gabriel.rosa15@outlook.com>
2023-09-13 15:02:23 -0300

    crud em python

--- brain.py
+++ brain.py
@@ -1,25 +1,15 @@
-import TipoPergunta
-import CalculaSentimento
-import RecoTheme
-import Resposta_Audio
 import RecoFala
+import ArvoreDeDecisao as chooses
 
 FalaOuTexto = input("Coloque:\nT = texto\nF = Fala\n")
 
 if str(FalaOuTexto).upper() == "T":
     prompt = input("Faça uma pergunta: ")
-    
-    strFeel, feel = CalculaSentimento.CalcFeeling(prompt)
-    QuestionType = TipoPergunta.QuestionNeuron().QuestionType(prompt)
-    theme = RecoTheme.RecoKeys(prompt)
-    
-    print(strFeel, feel)
-    print(QuestionType)
-    print("Tema: ", theme)
+    chooses.choose(prompt)
 else:
     RecoFala.voiceRecord(True)
                 
 # ------------------------------Informações Legais------------------------------
 # Projeto iniciado a aproximadamente 1 semana, hoje é dia 26 de agosto de 2023
 # Total de horas estará errado pois comeceu a contar ontem a noite, porém é legal pra ter uma idéia
-# Total de horas gastas no projeto: 14hr11min
+# Total de horas gastas no projeto: 15hr45min
