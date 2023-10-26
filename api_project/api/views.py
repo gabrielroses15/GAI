3581aedbdb73517f8577719e64e1485c21f0f061
LightRoses <gabriel.rosa15@outlook.com>
2023-10-26 12:32:17 -0300

    API básica criada

---
+++ api_project/api/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from .models import Pergunta
+from .serializers import PerguntaSerializer
+
+class PerguntaListView(generics.ListCreateAPIView):
+    queryset = Pergunta.objects.all()
+    serializer_class = PerguntaSerializer
+
+class PerguntaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pergunta.objects.all()
+    serializer_class = PerguntaSerializer
+from rest_framework import status
+from rest_framework.response import Response
+
+class PerguntaListView(generics.ListCreateAPIView):
+    queryset = Pergunta.objects.all()
+    serializer_class = PerguntaSerializer
+
+    def create(self, request, *args, **kwargs):
+        pergunta = request.data.get('pergunta', None)
+
+        if pergunta is not None and pergunta != "":
+            import sys
+            sys.path.append('C:\\Users\\gabriel.rosa\\Desktop\\GAI')
+            import brain
+            resposta = brain.APIrequest(pergunta)
+
+            # Cria um novo objeto Pergunta
+            nova_pergunta = Pergunta(pergunta=pergunta, resposta=resposta)
+            nova_pergunta.save()
+
+            # Serializa e retorna a nova pergunta com a resposta gerada
+            serializer = PerguntaSerializer(nova_pergunta)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'error': 'A pergunta não foi fornecida.'}, status=status.HTTP_400_BAD_REQUEST)
