3e55c604a55621ef86e5ee90846783368cb83163
LightRoses <gabriel.rosa15@outlook.com>
2023-11-01 13:08:24 -0300

    for push

--- DecisionModules/StrongVerbs.py
+++ DecisionModules/StrongVerbs.py
@@ -29,7 +29,6 @@ def StrongVerbs(frase: str, actionVerbs: dict, dicio, words: list, nomes:list, i
                 if word == nome and word not in nomesEncontrados:
                     nomesEncontrados.append(word)
                     break
-            #Mostrar pro greg
             # for nome in nomes:
             #     if nome in words[words.index(word) - 3 : words.index(word) + 3]:
             #         print(nome)
